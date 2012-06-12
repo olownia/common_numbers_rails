4eb97c67df43ab577c83618b7fb80d9335666208
Mariusz Ołownia <ollownia@gmail.com>
2012-06-12 12:12:42 +0200

    i18n in nip validator

--- lib/common_numbers_rails/nip_validator.rb
+++ lib/common_numbers_rails/nip_validator.rb
@@ -4,10 +4,7 @@ require 'active_model'
 class NipValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    raw_value = record.send(before_type_cast) if record.respond_to?(before_type_cast.to_sym)
-    raw_value ||= value
-
-    record.errors.add(attr_name, :not_a_nip, filtered_options(raw_value)) unless CommonNumbers::Polish::Nip.new(value).valid?
+    record.errors.add(attr_name, :not_a_nip, filtered_options(value)) unless CommonNumbers::Polish::Nip.new(value).valid?
   end
 
 end
